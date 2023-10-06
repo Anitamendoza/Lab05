e4a28a159bf0d0079d360d33cf5cfa5e749a9695
Cristina <ana.mendoza@tecsup.edu.pe>
2023-10-06 15:24:10 -0500

    Subiendo cambios

---
+++ Lab05/Lab05/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Lab05
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //this.dtPrueba.MaximumDate= DateTime.Now;
+            //dtPrueba.MaximumDate = DateTime.Now;
+
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ButtonDemo());
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ButtonCode());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new DatepickerDemo());
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new EditorDemo());
+            };
+            Item5.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new EditorCode());
+            };
+            Item6.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new EntryDemo());
+            };
+            Item7.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new EntryCode());
+            };
+            Item8.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new LabelDemo());
+            };
+            Item9.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new LabelCode());
+            };
+            Item10.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new LocalImage());
+            };
+            Item11.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new EmbeddedImage());
+            };
+        }
+    }
+}
